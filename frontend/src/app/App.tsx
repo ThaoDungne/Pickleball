79764eaad01f065101f4ed5f54902ab266f13701
ThaoDungne <dungtt7143@ut.edu.vn>
2025-05-23 23:31:33 +0700

    init

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <p className="text-red-500 text-5xl ">hello</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
